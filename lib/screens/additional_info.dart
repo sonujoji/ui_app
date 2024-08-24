3d7555a4742389615179cb75652763b8ef88e8ca
sonujoji <sonujoji22@gmail.com>
2024-08-24 10:28:55 +0530

    third

--- lib/screens/additional_info.dart
+++ lib/screens/additional_info.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class ScreenAdditionalInfo extends StatelessWidget {
+class ScreenAdditionalInfo extends StatefulWidget {
   const ScreenAdditionalInfo({super.key});
+
+  @override
+  State<ScreenAdditionalInfo> createState() => _ScreenAdditionalInfoState();
+}
+
+class _ScreenAdditionalInfoState extends State<ScreenAdditionalInfo> {
+  bool _isSwitched = true;
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +26,63 @@ class ScreenAdditionalInfo extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Center(
-        child: Text('ScreenAdditionalInfo'),
+      body: Column(
+        children: [
+          SizedBox(height: 15),
+          newListTile(
+            leadingIcon: Icons.share_outlined,
+            title: 'Share Dukaan App',
+            trailingIcon: Icons.arrow_forward_ios,
+          ),
+          newListTile(
+              leadingIcon: Icons.chat_bubble_outline,
+              title: 'Change Language',
+              trailingIcon: Icons.arrow_forward_ios),
+          newListTile(
+            leadingIcon: FontAwesomeIcons.whatsapp,
+            title: 'WhatsApp Chat Support',
+            trailingButton: Switch(
+                value: _isSwitched,
+                activeColor: Colors.blue,
+                onChanged: (value) {
+                  setState(() {
+                    _isSwitched = value;
+                  });
+                }),
+          ),
+          newListTile(
+              leadingIcon: Icons.lock_outlined, title: 'Privacy Policy'),
+          newListTile(leadingIcon: Icons.star_border, title: 'Rate Us'),
+          newListTile(leadingIcon: Icons.logout, title: 'Sign Out'),
+          SizedBox(
+            height: 200,
+          ),
+          Center(
+              child: Text(
+            "Version",
+            style: TextStyle(color: Colors.grey.shade300),
+          )),
+          Center(
+            child: Text(
+              '2.4.2',
+              style: TextStyle(
+                  color: Colors.grey.shade500, fontWeight: FontWeight.w500),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  ListTile newListTile(
+      {required IconData leadingIcon,
+      required String title,
+      IconData? trailingIcon,
+      Switch? trailingButton}) {
+    return ListTile(
+        leading: Icon(leadingIcon),
+        title: Text(title),
+        trailing: trailingButton ??
+            (trailingIcon != null ? Icon(trailingIcon) : null));
   }
 }
--- lib/screens/manage_store.dart
+++ lib/screens/manage_store.dart
@@ -6,17 +6,60 @@ class ScreenManageStore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Manage Store",
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Manage Store",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
         ),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Text('ScreenManageStore'),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Container(
+                    color: Colors.green,
+                    height: 110,
+                    width: 150,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            'assets/images/marketing.jpg',
+                            width: 40,
+                            fit: BoxFit.cover,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Marketing\nDesigns',
+                            textAlign: TextAlign.left,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 100,
+                    width: 150,
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
