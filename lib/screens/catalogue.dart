8f36105dab478e2a690034a9120c478fb8dce3a6
sonujoji <sonujoji22@gmail.com>
2024-09-12 13:06:49 +0530

    new

--- lib/screens/catalogue.dart
+++ lib/screens/catalogue.dart
@@ -1,30 +1,235 @@
 import 'package:flutter/material.dart';
 
-class ScreenCatalogue extends StatelessWidget {
+class ScreenCatalogue extends StatefulWidget {
   const ScreenCatalogue({super.key});
 
   @override
+  State<ScreenCatalogue> createState() => _ScreenCatalogueState();
+}
+
+class _ScreenCatalogueState extends State<ScreenCatalogue> {
+  bool switchButton = true;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Catalogue",
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blue,
-        actions: [
-          Icon(
-            Icons.search,
-            color: Colors.white,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              "Catalogue",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue,
+            bottom: TabBar(
+                indicatorColor: Colors.white,
+                indicatorPadding: EdgeInsets.symmetric(horizontal: -60),
+                tabs: [
+                  Tab(
+                    child: Text(
+                      "Products",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                  Text(
+                    "Categories",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ]),
+            actions: [
+              Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+            ],
           ),
-          SizedBox(
-            width: 15,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 7, right: 7, bottom: 10),
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Card(
+                    elevation: .5,
+                    child: catalogueListView(
+                        image: 'assets/products/shirt.png',
+                        title: 'Couch Potato | Men | Bl..',
+                        sizedBoxWidth: 19,
+                        price: '799'),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Card(
+                    elevation: .5,
+                    child: catalogueListView(
+                        image: 'assets/products/cup1.png',
+                        title: 'Mug | Explore',
+                        sizedBoxWidth: 90,
+                        price: '399'),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Card(
+                    elevation: .5,
+                    child: catalogueListView(
+                        image: 'assets/products/combo.webp',
+                        sizedBoxWidth: 30,
+                        title: 'Combo Blast 1 | Pack..',
+                        price: '699'),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Card(
+                    elevation: .5,
+                    child: catalogueListView(
+                        image: 'assets/products/cup.png',
+                        sizedBoxWidth: 86,
+                        title: 'Mug | Orchard',
+                        price: '699'),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Card(
+                    elevation: .5,
+                    child: catalogueListView(
+                        image: 'assets/products/combo.webp',
+                        title: 'Combo Blast2 | Pack',
+                        sizedBoxWidth: 43,
+                        price: '699'),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Card(
+                    elevation: .5,
+                    child: catalogueListView(
+                        image: 'assets/products/2ndtshirt.jpg',
+                        title: 'I See Combo Pack',
+                        sizedBoxWidth: 60,
+                        price: '1,299'),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Card(
+                    elevation: .5,
+                    child: catalogueListView(
+                        image: 'assets/products/combo.webp',
+                        title: 'Kids Combo Blast',
+                        sizedBoxWidth: 62,
+                        price: '1,299'),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+
+  Container catalogueListView(
+      {required String image,
+      required String title,
+      required String price,
+      required double sizedBoxWidth}) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade200),
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 12, top: 12, right: 12),
+            child: Row(
+              children: [
+                Container(
+                  foregroundDecoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                  height: 100,
+                  width: 100,
+                  child: Image.asset(
+                    image,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(title),
+                          SizedBox(width: sizedBoxWidth),
+                          Icon(
+                            Icons.more_vert_outlined,
+                            color: Colors.grey.shade500,
+                          )
+                        ],
+                      ),
+                      Text('1piece'),
+                      // SizedBox(
+                      //   height: 10,
+                      // ),
+                      Text(
+                        ' ₹$price',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'In stock',
+                            style: TextStyle(color: Colors.green.shade400),
+                          ),
+                          SizedBox(width: 90),
+                          Switch(
+                              value: switchButton,
+                              activeColor: Colors.blue,
+                              onChanged: (value) {
+                                setState(() {
+                                  switchButton = value;
+                                });
+                              })
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Divider(
+              color: Colors.grey.shade200,
+              thickness: 1.5,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 35,
+            child: Row(
+              children: [
+                SizedBox(width: 120),
+                Icon(Icons.share_outlined),
+                Text(' Share Product'),
+              ],
+            ),
           ),
         ],
-      ),
-      body: Center(
-        child: Text('ScreenCatalogue'),
       ),
     );
   }
--- lib/screens/manage_store.dart
+++ lib/screens/manage_store.dart
@@ -15,7 +15,7 @@ class ScreenManageStore extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.all(10),
         child: Column(
           children: [
             SizedBox(
@@ -23,11 +23,17 @@ class ScreenManageStore extends StatelessWidget {
             ),
             Row(
               children: [
-                cardView('assets/images/marketing.jpg', 'Marketing\nDesigns'),
-                SizedBox(
-                  width: 20,
+                Card(
+                  child: cardView(Icons.campaign_outlined,
+                      Colors.orange.shade400, 'Marketing\nDesigns'),
                 ),
-                cardView('assets/images/rupees.jpg', 'Online\nPayments')
+                SizedBox(
+                  width: 10,
+                ),
+                Card(
+                  child: cardView(Icons.currency_rupee_sharp,
+                      Colors.green.shade400, 'Online\nPayments'),
+                )
               ],
             ),
             SizedBox(
@@ -35,11 +41,17 @@ class ScreenManageStore extends StatelessWidget {
             ),
             Row(
               children: [
-                cardView('assets/images/discount.jpg', 'Discount\nCoupons'),
-                SizedBox(
-                  width: 20,
+                Card(
+                  child: cardView(Icons.local_offer, Colors.orange.shade200,
+                      'Discount\nCoupons'),
                 ),
-                cardView('assets/images/myCustomers.jpg', 'My\nCustomers')
+                SizedBox(
+                  width: 10,
+                ),
+                Card(
+                  child: cardView(
+                      Icons.people, Colors.blue.shade300, 'My\nCustomers'),
+                )
               ],
             ),
             SizedBox(
@@ -47,11 +59,17 @@ class ScreenManageStore extends StatelessWidget {
             ),
             Row(
               children: [
-                cardView('assets/images/scan.png', 'Store QR\nCode'),
-                SizedBox(
-                  width: 20,
+                Card(
+                  child: cardView(Icons.qr_code_scanner_outlined, Colors.grey,
+                      'Store QR\nCode'),
                 ),
-                cardView('assets/images/rupees.png', 'Extra\nCharges')
+                SizedBox(
+                  width: 10,
+                ),
+                Card(
+                  child: cardView(Icons.money, Colors.deepPurple.shade400,
+                      'Extra\nCharges'),
+                )
               ],
             ),
             SizedBox(
@@ -59,50 +77,60 @@ class ScreenManageStore extends StatelessWidget {
             ),
             Row(
               children: [
-                Container(
-                  height: 110,
-                  decoration: BoxDecoration(
-                      color: Colors.white70,
-                      border: Border.all(color: Colors.white30),
-                      borderRadius: BorderRadius.circular(10)),
-                  width: 155,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.asset(
-                              'assets/images/orderForm.jpg',
-                              width: 40,
-                              fit: BoxFit.cover,
-                            ),
-                            SizedBox(
-                              width: 45,
-                            ),
-                            Container(
-                              width: 45,
-                              height: 20,
-                              child: Text(
-                                '  NEW',
-                                style: TextStyle(color: Colors.white),
+                Card(
+                  child: Container(
+                    height: 110,
+                    decoration: BoxDecoration(
+                        color: Colors.white70,
+                        border: Border.all(color: Colors.white30),
+                        borderRadius: BorderRadius.circular(10)),
+                    width: 155,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                    color: Colors.purple.shade300,
+                                    borderRadius: BorderRadius.circular(6)),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.format_list_bulleted_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ),
                               ),
-                              decoration: BoxDecoration(
-                                  color: Colors.green.shade500,
-                                  borderRadius: BorderRadius.circular(5)),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          'Order\nForm',
-                          textAlign: TextAlign.left,
-                        )
-                      ],
+                              SizedBox(
+                                width: 45,
+                              ),
+                              Container(
+                                width: 45,
+                                height: 20,
+                                child: Text(
+                                  '  NEW',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                decoration: BoxDecoration(
+                                    color: Colors.green.shade500,
+                                    borderRadius: BorderRadius.circular(5)),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Order\nForm',
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -128,7 +156,7 @@ class ScreenManageStore extends StatelessWidget {
     );
   }
 
-  Container cardView(String image, String text) {
+  Container cardView(IconData icon, Color color, String text) {
     return Container(
       height: 110,
       decoration: BoxDecoration(
@@ -141,10 +169,17 @@ class ScreenManageStore extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(
-              image,
+            Container(
+              height: 40,
               width: 40,
-              fit: BoxFit.cover,
+              decoration: BoxDecoration(
+                  color: color, borderRadius: BorderRadius.circular(6)),
+              child: Center(
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                ),
+              ),
             ),
             SizedBox(
               height: 5,
